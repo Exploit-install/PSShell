ef9ef950da0d87e26c092e0fa7744eae2da7b6e6
fdiskyou <fdiskyou@users.noreply.github.com>
2016-05-15 10:18:35 +0100

    1st import into tree

---
+++ PSShell/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Host;
+using System.Management.Automation.Runspaces;
+
+namespace PSShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "PSShell - rui@deniable.org";
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            string command = null;
+
+            do
+            {
+                Console.Write("PS > ");
+                command = Console.ReadLine();
+
+                try
+                {
+                    Pipeline pipeline = runspace.CreatePipeline();
+                    pipeline.Commands.AddScript(command);
+                    pipeline.Commands.Add("Out-String");
+                    Collection<PSObject> results = pipeline.Invoke();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    foreach (PSObject obj in results)
+                    {
+                        stringBuilder.AppendLine(obj.ToString());
+                    }
+                    Console.Write(stringBuilder.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("{0}", e.Message);
+                }
+            } while (command != "exit");
+
+            runspace.Close();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# PSShell
+PSShell gets the job done when harsh group policy restrictions are in place.
+
+### What is it:
+
+PSShell is an application written in C# that does not rely on powershell.exe but runs powershell commands and functions within a powershell runspace environment (.NET). It doesn't need to be "installed" so it's very portable.
+
+Because it calls powershell directly through the .NET framework it might help bypassing security controls like GPO, SRP, App Locker.
+
+### How to Compile it:
+
+To compile PSShell you need to import this project within Microsoft Visual Studio or if you don't have access to a Visual Studio installation, you can compile it as follows:
+
+To Compile as x86 binary:
+
+```
+cd C:\Windows\Microsoft.NET\Framework64\v4.0.30319 (Or newer .NET version folder)
+
+csc.exe /unsafe /reference:"C:\path\to\System.Management.Automation.dll" /reference:System.IO.Compression.dll /out:C:\users\username\PowerOPS_x86.exe /platform:x86 "C:\path\to\PowerOPS\PowerOPS\*.cs"
+```
+
+To Compile as x64 binary:
+
+```
+cd C:\Windows\Microsoft.NET\Framework64\v4.0.30319 (Or newer .NET version folder)
+
+csc.exe /unsafe /reference:"C:\path\to\System.Management.Automation.dll" /reference:System.IO.Compression.dll /out:C:\users\username\PowerOPS_x64.exe /platform:x64 "C:\path\to\PowerOPS\PowerOPS\*.cs"
+```
+
+PSShell uses the System.Management.Automation namespace, so make sure you have the System.Management.Automation.dll within your source path when compiling outside of Visual Studio.
+
+### How to use it:
+
+Just run the executables.
